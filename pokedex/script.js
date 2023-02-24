0a5add5ea9c18b4e813b9c59e4b0466a3fe703f5
DamienPalm <palm.damien@gmail.com>
2023-02-24 22:52:33 +0100

    maj 24/02/2023

--- pokedex/script.js
+++ pokedex/script.js
@@ -24,7 +24,7 @@ const afficherPokemon = (pokemon) => {
     const pokemonHTML = pokemon.map(poke => `
         <div class="card col-10 col-md-5 col-lg-3 m-1 p-2">
             <img src="${poke.image}" class="card-img-top"/>
-            <div class="card-body">
+            <div class="card-body rounded-b-lg">
                 <p class="card-text text-secondary">No.${poke.id}</p>
                 <h2 class="card-title">${poke.name}</h2>
                 <p class="card-text">${poke.type}</p>
@@ -35,3 +35,14 @@ const afficherPokemon = (pokemon) => {
 };
 
 fetchPokemon();
+
+// const fetchPokemon = function () {
+//     const url = `https://pokeapi.co/api/v2/pokemon/1`;
+//     fetch(url)
+//         .then(res => {
+//             return res.json();
+//         })
+//         .then(data => {
+//             console.log(data);
+//         });
+// };
